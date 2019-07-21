56fdf04a036801dec51202aad16c0f02705dce52
Shalom Craimer <Shalom.Craimer@LTTS.com>
2019-07-21 09:34:09 +0300

    Add MutationObserver to reapply the filters during infinite scroll of the page, which adds new items (some of which need to be filtered)

--- src/content-script.js
+++ src/content-script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SECONDS_TO_WAIT_UNTIL_FILTER = 5;
+var SECONDS_TO_WAIT_UNTIL_FILTER_AFTER_LOAD = 3;
 var CLASSNAME_OF_CONTAINER = "section__items";
 
 function find_ancestor_with_className(elem, className)
@@ -17,6 +18,7 @@ function find_ancestor_with_className(elem, className)
 function hide_purchased()
 {
 	var purchased_bylines = document.getElementsByClassName("store-section-item-byline__purchased");
+	var cells_to_remove = [];
 	for( var i in purchased_bylines )
 	{
 		var sub_element = purchased_bylines[i];
@@ -25,8 +27,13 @@ function hide_purchased()
 		{
 			continue;
 		}
-		cell.parentElement.removeChild( cell );
+		cells_to_remove.push( cell );
 	}
+	cells_to_remove.forEach(cell => {
+		console.log("Purchased. Removing: ");
+		console.log(cell);
+		cell.parentElement.removeChild( cell );
+	});
 }
 
 function get_section_oculus_id( section )
@@ -65,20 +72,6 @@ function hide_items_from_list( list_of_oculus_store_ids )
 
 function apply_filters()
 {
-	/*
-	var dom_container = (function(){
-		var containers = document.getElementsByClassName(CLASSNAME_OF_CONTAINER);
-		return ( containers.length > 0 ) ?  containers[0] : null;
-	})();
-
-	if( dom_container == null )
-	{
-		return false;
-	}
-	
-	console.log(dom_container);
-	*/
-	
 	hide_purchased();
 	
 	var list = [
@@ -88,28 +81,62 @@ function apply_filters()
 		1257633284291446
 	];
 	hide_items_from_list( list );
-	
-	return true;
 }
 
-function apply_filters_proxy()
+var observer = null;
+var delayedApplyHandle = null;
+
+function postpone_filtering_of_items()
 {
-	var result = apply_filters();
-	if( result )
+	if( delayedApplyHandle )
 	{
-		console.log("Filter of Oculus Store items: filters applied successfully.");
+		clearTimeout(delayedApplyHandle);
 	}
-	else
-	{
-		console.log("Filter of Oculus Store items: filters were not applied.");
-	}
+	
+	delayedApplyHandle = setTimeout( function() {
+		console.log("Applying filters...");
+		apply_filters();
+		delayedApplyHandle = null;
+	}, SECONDS_TO_WAIT_UNTIL_FILTER_AFTER_LOAD * 1000);
 }
+
+function init_mutation_observer()
+{
+	var dom_container = (function(){
+		var containers = document.getElementsByClassName(CLASSNAME_OF_CONTAINER);
+		return ( containers.length > 0 ) ?  containers[0] : null;
+	})();
+
+	if( dom_container == null )
+	{
+		return false;
+	}
+
+	const mutationConfig = { attributes: false, childList: true, subtree: false, characterData: false,
+    	characterDataOldValue: false};
+
+	var onMutate = function(mutationsList) {
+    	mutationsList.forEach(mutation => {
+			if( mutation.type != "childList" )
+			{
+				return;
+			}
+			mutation.addedNodes.forEach(newNode => {
+        		postpone_filtering_of_items();
+			});
+    	});
+	};
+	
+	var observer = new MutationObserver(onMutate);
+	observer.observe(dom_container, mutationConfig);	
+}
+
 
 function onload()
 {
 	console.log("Filter of Oculus Store items: loaded.");
 	
-	setTimeout(apply_filters_proxy, SECONDS_TO_WAIT_UNTIL_FILTER * 1000);
+	setTimeout(function(){ apply_filters(); init_mutation_observer(); }, SECONDS_TO_WAIT_UNTIL_FILTER * 1000);
 }
 
 jQuery(document).ready(function() {
