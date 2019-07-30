347eef1b0f4d23438e22f3bde1b889364f523c0e
Shalom Craimer <Shalom.Craimer@LTTS.com>
2019-07-30 11:14:39 +0300

    Add UI for blacklist button

--- src/add-items-to-blacklist-content-script.js
+++ src/add-items-to-blacklist-content-script.js
@@ -33,6 +33,72 @@ function init_mutation_observer()
 }
 */
 
+function isGameInBlacklist( gameId )
+{
+	// TODO: this is a stub
+	return false;
+}
+
+function getGameId()
+{
+	var experienceRegex = RegExp('^https://www.oculus.com/experiences/rift/([0-9]+)/?$');
+	var matches = document.URL.match(experienceRegex);
+	if( !matches || matches.length < 2 )
+	{
+		return null;
+	}
+	
+	return matches[1];
+}
+
+function addButton()
+{
+	var gameId = getGameId();
+	if( !gameId )
+	{
+		console.log("Error finding Game ID");
+		return;
+	}
+
+	var buttons = document.getElementsByClassName('app-wishlist-button');
+	if( !buttons || buttons.length < 2 )
+	{
+		console.log("Error finding wishlist button");
+		return;
+	}
+
+	var wishlistButton = buttons[1];
+	var parentElement = wishlistButton.parentElement;
+	var blacklistButton = wishlistButton.cloneNode(true);
+	parentElement.appendChild( blacklistButton );
+
+	var buttonAttrs = (function() {
+		if( isGameInBlacklist( gameId ) )
+		{
+			return {
+				//iconClassName: "wishlist-icon wishlist-icon__hover wishlist-icon__remove",
+				iconClassName: "wishlist-icon wishlist-icon__remove",
+				label: "Remove from Blacklist"
+			};
+		}
+		else
+		{
+			return {
+				iconClassName: "wishlist-icon wishlist-icon__add",
+ 				label: "Add to Blacklist"
+			};
+		}
+	})();
+	
+	console.log(buttonAttrs);
+	
+	var iconElem = blacklistButton.childNodes[0].firstChild;
+	var labelElem = blacklistButton.childNodes[1];
+	
+	iconElem.className = buttonAttrs.iconClassName;
+	labelElem.innerText = buttonAttrs.label;
+}
+
 function isPageOfGame()
 {
 	var isExperienceRegex = RegExp('^https://www.oculus.com/experiences/rift/[0-9]+/?$');
@@ -48,9 +114,8 @@ function onload()
 	}
 	console.log("Oculus Store content script for adding items to blacklist: loaded.");
 	
-	/*
-	setTimeout(function(){ apply_filters(); init_mutation_observer(); }, SECONDS_TO_WAIT_UNTIL_FILTER * 1000);
-	*/
+	var delayToAddingButton = 3;
+	setTimeout(function(){ addButton(); }, delayToAddingButton * 1000);
 }
 
 jQuery(document).ready(function() {
